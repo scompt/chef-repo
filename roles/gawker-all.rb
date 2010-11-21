97961f6f6c2b87f60fa998c28874df5c9df40451
Edward Dale <scompt@scompt.com>
2010-11-21 20:50:14 +0100

    Tweaking roles.

--- roles/gawker-all.rb
+++ roles/gawker-all.rb
@@ -1,7 +1,7 @@
-name "webserver"
-description "Example that runs a web server."
+name "gawker-all"
+description "FoodGawker role."
 # List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
-run_list("webserver")
+run_list("apt", "wordpress")
 # Attributes applied if the node doesn't have it set already.
 #default_attributes()
 # Attributes applied no matter what the node has set already.
